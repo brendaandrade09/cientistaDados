e055f49c1e5ddc627eb4071f1e16884224014a23
brendaandrade09 <brenda.andrade@edu.senai.br>
2026-06-16 16:42:09 -0300

    Add files via upload

---
+++ cientistaDados/roboEmail.py
@@ -0,0 +1,22 @@
+import pyautogui
+import pyperclip
+import webbrowser
+import time
+
+email = 'brenda.andrade@edu.senai.br'
+assunto = 'Relatório mensal de faturamento'
+
+webbrowser.open('mail.google.com')
+time.sleep (8)
+pyautogui.click(x=145, y=172)
+time.sleep (8)
+pyperclip.copy(email)
+pyautogui.hotkey('ctrl','v')
+time.sleep (3)
+pyautogui.press('enter')
+pyautogui.press('tab')
+time.sleep (3)
+pyperclip.copy(assunto)
+pyautogui.hotkey('ctrl','v')
+pyautogui.press('enter')
+pyautogui.press('tab')
